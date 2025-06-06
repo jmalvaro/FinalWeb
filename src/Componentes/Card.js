9a737221577b7a21c4b8034260511425108f4036
Alvaro Martinez <amartinez9617@gmail.com>
2025-06-06 15:05:44 -0500

    SegundoCommit

---
+++ src/Componentes/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <div className="card">
+        
+        <h2>Título de la tarjeta</h2>
+        <p>Contenido de la tarjeta</p>
+    </div>
+  );
+};
+
+export default Card;
